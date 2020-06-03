94d682acbdb5aabc50c249892a5319f6b438e1cc
Ciremun <Ciremun@yandex.ru>
2020-06-04 02:38:42 +0300

    handle channel deleted event

--- bot/client.py
+++ bot/client.py
@@ -18,4 +18,26 @@ async def on_message(message):
         except (KeyError, TypeError):
             return
 
+
+@client.event
+async def on_guild_channel_delete(channel):
+    db.disconnect_channel(channel.id)
+    streams = db.getStreams()
+    for username, userdata in streams.items():
+        channels = userdata['channels']
+        if channel.id not in channels:
+            continue
+        userid = userdata['userid']
+        newChannels = channels.copy()
+        channels = [str(x) for x in channels]
+        try:
+            newChannels.remove(channel.id)
+            if not newChannels:
+                db.removeNotify(username, ', '.join(channels), userid=userid)
+                continue
+            newChannels = [str(x) for x in newChannels]
+            db.updateNotifyChannels(username, ', '.join(channels), ', '.join(newChannels), userid=userid)
+        except ValueError:
+            pass
+
 client.run(tokens['DiscordToken'])
