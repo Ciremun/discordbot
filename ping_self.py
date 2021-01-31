be5a7a97e5cdd04b7ce4ae3136f4d9d1a39e3339
Ciremun <Ciremun@yandex.ru>
2021-01-31 16:13:16 +0300

    more pingsgit add -A

--- ping_self.py
+++ ping_self.py
@@ -1,6 +1,6 @@
 import os
 import time
-from random import randrange
+from random import uniform
 from os.path import join, dirname
 
 import requests
@@ -11,5 +11,5 @@ load_dotenv(join(dirname(__name__), '.env'))
 self_url = os.environ.get('CALLBACK_URL')
 
 while True:
-    time.sleep(randrange(8, 20) * 60)
+    time.sleep(uniform(5, 10) * 60)
     requests.get(self_url)
