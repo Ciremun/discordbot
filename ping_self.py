a3874d8b53e953971cb7a5a95572d53325880858
Ciremun <Ciremun@yandex.ru>
2021-02-05 00:37:23 +0300

    catch exceptions

--- ping_self.py
+++ ping_self.py
@@ -6,10 +6,15 @@ from os.path import join, dirname
 import requests
 from dotenv import load_dotenv
 
+from src.log import logger
+
 load_dotenv(join(dirname(__name__), '.env'))
 
 self_url = os.environ.get('CALLBACK_URL')
 
 while True:
     time.sleep(uniform(15, 25) * 60)
-    requests.get(self_url)
+    try:
+        requests.get(self_url)
+    except Exception as e:
+        logger.error(e)
