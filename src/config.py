637422eea81c3261cfcce78e39d347842ff3908b
Ciremun <Ciremun@yandex.ru>
2021-02-04 19:50:25 +0100

    Add TODO(#3)

--- src/config.py
+++ src/config.py
@@ -9,5 +9,6 @@ load_dotenv(join(dirname(__name__), '.env'))
 
 keys = {attr: os.environ.get(attr) for attr in [
     'CLIENT_ID', 'DISCORD_TOKEN', 'CLIENT_OAUTH', 'CLIENT_SECRET', 'APP_ACCESS_TOKEN', 'CALLBACK_URL', 'SECRET']}
+# TODO(#3): move cfg.json here
 cfg = json.load(open('cfg.json'))
 start_time = time.time()
